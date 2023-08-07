957f869437d33d206966edc4d4457521b16a5e5b
nguyentr02 <truongphamtuannguyen@gmail.com>
2023-08-07 18:39:39 +1000

    Adding component
    Create and Add component Route _ in _ app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { MarketComponent } from './market/market.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: LoginComponent,pathMatch:'full'},
+  {path:'register',component: RegisterComponent},
+  {path:'forgotPassword',component: ForgotPasswordComponent},
+  {path:'home',component:MarketComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
