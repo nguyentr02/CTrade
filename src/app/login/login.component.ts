8d6802bd40d886c417178e559e6ecdfe8acde58e
nguyentr02 <truongphamtuannguyen@gmail.com>
2023-08-07 18:42:12 +1000

    Generate and Edit login Component
    First version of Login Component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  
+})
+export class LoginComponent implements OnInit {
+  
+  constructor(private router: Router) {
+
+  }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('',[Validators.required]);
+  getErrorEmailMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  verifyAccount() {
+    this.router.navigate(['/home']);
+  }
+  getErrorPasswordMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('password');
+  }
+  async ngOnInit(): Promise<void> {
+    
+  }
+}
