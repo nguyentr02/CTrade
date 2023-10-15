66f19fdadba6ace3908c690123b86833b9356962
nguyentr02 <truongphamtuannguyen@gmail.com>
2023-10-15 20:47:13 +1100

    Add History Rendering
    Add History Render BUY/SELL Transactions

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,5 +1,5 @@
-import {Component, ViewEncapsulation} from '@angular/core';
-import {MatTabsModule} from '@angular/material/tabs';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 import { UserService } from '../services/user.service';
 import { TransactionService } from '../services/transaction.service';
 import { Router } from '@angular/router';
@@ -8,36 +8,54 @@ import { Router } from '@angular/router';
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class HistoryComponent {
   activeTabIndex = 0; // Default active tab index
 
-  constructor(    private router: Router,
-    private userService: UserService, private transactionService: TransactionService) {
-
-  }
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private transactionService: TransactionService
+  ) {}
   onTabChange(event: any) {
     this.activeTabIndex = event.index;
   }
-  transactions : any;
+  transactions: any;
+  buyTransactions: any = [];
+  sellTransactions: any = [];
   userID: any;
+  index: number = 0;
 
   async ngOnInit(): Promise<void> {
     if (sessionStorage.length == 0) {
-      window.alert("Need to Login first!")
+      window.alert('Need to Login first!');
       this.router.navigate(['/']);
     }
     this.userID = parseInt(sessionStorage.getItem('userID'));
-    console.log(this.userID)
+    console.log(this.userID);
 
+    // Get Data for All Transaction from User
     try {
-      this.transactions = await this.transactionService.getTransactionsByID(this.userID)
+      this.transactions = await this.transactionService.getTransactionsByID(
+        this.userID
+      );
     } catch (error) {
       console.log(error);
     }
-    console.log(this.transactions)
-    console.log(this.userID)
-  }
+    // console.log(this.transactions)
+    // console.log(this.userID)
 
+    // Get Data for BUY Transaction from User
+    // Get Data fro SELL Transaction from User
+    for (let i = 0; i < this.transactions.length; i++) {
+      console.log(this.transactions[i].TransMethod);
+      // If transaction method is BUY then push transaction[i] into buyTransaction
+      if (this.transactions[i].TransMethod == 'Buy') {
+        this.buyTransactions.push(this.transactions[i]);
+      } else {
+        this.sellTransactions.push(this.transactions[i]);
+      }
+    }
+  }
 }
